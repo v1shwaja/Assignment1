682af9379e0b6594d03b72d3cf5e7d54578abff2
v1shwaja <vishwaja.pinjarkar@gmail.com>
2025-06-11 23:22:31 +0530

    Add files via upload

---
+++ arithematic.py
@@ -0,0 +1,12 @@
+n1=input('Enter the first number=')
+n1=int(n1)
+n2=input('Enter the second number=')
+n2=int(n2)
+A=n1+n2
+print('Addition=',A)
+S=n1-n2
+print('Subtraction=',S)
+M=n1*n2
+print('Multiplication=',M)
+D=n1/n2
+print('Division=',D)
---
+++ greeting.py
@@ -0,0 +1,3 @@
+Fname=input('Enter your First Name: ')
+Lname=input('Enter your Last Name: ')
+print('Hello',Fname,Lname+'!' + ' Welcome'+' to'+' the'+' python'+' program.')
